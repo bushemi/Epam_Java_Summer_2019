a467bc44df55a6576835b38a88e8426b0a1e0633
Igor_Boldysh <bushemi2001@gmail.com>
2019-07-21 20:23:04 +0300

    HT-2. Was implemented module with an interface for a calculator's core.

---
+++ calc-console/calc-interface/src/main/java/com/epam/calc/interfaces/Calc.java
@@ -0,0 +1,15 @@
+package com.epam.calc.interfaces;
+
+/**
+ * Main interface for calculation operations
+ */
+public interface Calc {
+    double addition(double a, double b);
+
+    double subtraction(double a, double b);
+
+    double multiplication(double a, double b);
+
+    double division(double a, double b);
+
+}
