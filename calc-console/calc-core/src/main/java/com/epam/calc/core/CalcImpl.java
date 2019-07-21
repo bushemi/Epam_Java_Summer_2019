ddd56651a8a8a066081130e25af4e9492bde1cae
Igor_Boldysh <bushemi2001@gmail.com>
2019-07-21 20:44:41 +0300

    HT-2. Was implemented module with a core functionality for a calculator.

---
+++ calc-console/calc-core/src/main/java/com/epam/calc/core/CalcImpl.java
@@ -0,0 +1,22 @@
+package com.epam.calc.core;
+
+import com.epam.calc.interfaces.Calc;
+
+public class CalcImpl implements Calc {
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+}
