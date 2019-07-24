439cccfe8941f923c8fa5ef4c290a525c064c9ec
Igor_Boldysh <bushemi2001@gmail.com>
2019-07-24 19:33:48 +0300

    HT-2. Was implemented operations for calculations in console module.

---
+++ calculator/calc-console/src/main/java/com/epam/Operations.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+class Operations {
+    static final String ADDITION = "+";
+    static final String SUBTRACTION = "-";
+    static final String MULTIPLICATION = "*";
+    static final String ALTERNATE_MULTIPLICATION = "x";
+    static final String DIVISION = "/";
+
+    private Operations() {
+    }
+}
