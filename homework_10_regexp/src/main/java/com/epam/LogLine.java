05be2d4af28f572a8b077feaf977e7c71b5b4393
Igor_Boldysh <bushemi2001@gmail.com>
2019-09-09 19:50:33 +0300

    HT-10. Were added methods for comparing log lines.

--- homework_10_regexp/src/main/java/com/epam/LogLine.java
+++ homework_10_regexp/src/main/java/com/epam/LogLine.java
@@ -1,6 +1,14 @@
 package com.epam;
 
-public class LogLine {
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Basic line of parsed logs
+ * <p>
+ * Note: this class has a natural ordering that is inconsistent with equals.
+ */
+public class LogLine implements Serializable, Comparable {
     private String dateTime;
     private String module;
     private String operation;
@@ -39,6 +47,29 @@ public class LogLine {
 
     public void setExecutionTimeMsec(String executionTimeMsec) {
         this.executionTimeMsec = executionTimeMsec;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        LogLine logLine = (LogLine) o;
+        Integer executionTimeObjects = Integer.parseInt(logLine.getExecutionTimeMsec());
+        Integer executionTimeCurrent = Integer.parseInt(this.getExecutionTimeMsec());
+        return executionTimeObjects - executionTimeCurrent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LogLine)) return false;
+        LogLine logLine = (LogLine) o;
+        return Objects.equals(getDateTime(), logLine.getDateTime()) &&
+                Objects.equals(getOperation(), logLine.getOperation()) &&
+                Objects.equals(getExecutionTimeMsec(), logLine.getExecutionTimeMsec());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDateTime(), getOperation(), getExecutionTimeMsec());
     }
 
     @Override
