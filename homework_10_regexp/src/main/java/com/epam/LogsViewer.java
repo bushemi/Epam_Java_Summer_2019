bf637db2f4a3d62bbc7b497e8aff50f032e011d7
Igor_Boldysh <bushemi2001@gmail.com>
2019-09-09 19:50:01 +0300

    HT-10. Was added logs viewer with tests.

--- homework_10_regexp/src/main/java/com/epam/LogsViewer.java
+++ homework_10_regexp/src/main/java/com/epam/LogsViewer.java
@@ -1,7 +1,34 @@
 package com.epam;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class LogsViewer {
+
     public String createResultForView(LogLine line) {
-        return String.format("%s %s ms, finished at %s", line.getOperation(), line.getExecutionTimeMsec(), line.getDateTime());
+        return String.format("%s %s ms, finished at %s", line.getOperation(),
+                line.getExecutionTimeMsec(), line.getDateTime());
+    }
+
+    public Map<String, Set<LogLine>> getLongestOperationsByModule(List<LogLine> list, int count) {
+        Map<String, Set<LogLine>> result = new HashMap<>();
+        list.forEach(line -> includeLogToMap(result, line));
+        limitResults(result, count);
+        return result;
+    }
+
+    private void includeLogToMap(Map<String, Set<LogLine>> map, LogLine line) {
+        Set<LogLine> strings = map.getOrDefault(line.getModule(),
+                new TreeSet<>());
+        strings.add(line);
+        map.put(line.getModule(), strings);
+    }
+
+    private void limitResults(Map<String, Set<LogLine>> result, int count) {
+        Set<String> keys = result.keySet();
+        for (String key : keys) {
+            result.compute(key, (keyNotNeeded, value) ->
+                    new TreeSet<>(result.get(key).stream().limit(count).collect(Collectors.toSet())));
+        }
     }
 }
--- homework_10_regexp/src/test/java/com/epam/LogsViewerTest.java
+++ homework_10_regexp/src/test/java/com/epam/LogsViewerTest.java
@@ -2,12 +2,24 @@ package com.epam;
 
 import org.junit.Test;
 
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.epam.LogsReaderTest.LOG_FILE_NAME;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertNotNull;
 
 public class LogsViewerTest {
-    LogsViewer logViewer = new LogsViewer();
+    private LogsViewer logsViewer = new LogsViewer();
+    private LogsReader logsReader = new LogsReader();
+    private LogsParser logsParser = new LogsParser();
 
     @Test
     public void createResultForView() {
@@ -18,11 +30,40 @@ public class LogsViewerTest {
         logLine.setDateTime("1234-12-34 12:34:56:789");
 
         //WHEN
-
-        String resultForView = logViewer.createResultForView(logLine);
+        String resultForView = logsViewer.createResultForView(logLine);
 
         //THEN
         assertThat(resultForView, is(equalTo("test 111 ms, finished at 1234-12-34 12:34:56:789")));
     }
 
+    @Test
+    public void getLongestOperationsByModule() {
+        //GIVEN
+        List<String> logs = logsReader.read(Paths.get(LOG_FILE_NAME));
+        List<LogLine> logLines = logs.stream().map(logsParser::parseLogs).collect(Collectors.toList());
+
+        //WHEN
+        Map<String, Set<LogLine>> longestOperationsSortedByModule = logsViewer.getLongestOperationsByModule(logLines, 3);
+
+        //THEN
+        validateLongestOperation(longestOperationsSortedByModule, "ASC", "Logout", "47342");
+        validateLongestOperation(longestOperationsSortedByModule, "LMC", "Save Order", "35899");
+        validateLongestOperation(longestOperationsSortedByModule, "CMN", "Login", "42867");
+        validateLongestOperation(longestOperationsSortedByModule, "MIC", "Logout", "12258");
+        validateLongestOperation(longestOperationsSortedByModule, "LWS", "Login", "44823");
+        validateLongestOperation(longestOperationsSortedByModule, "LAB", "Find Product", "47543");
+    }
+
+    private void validateLongestOperation(Map<String, Set<LogLine>> longestOperationsSortedByModule,
+                                          String moduleName, String operation, String executionTime) {
+        Set<LogLine> logLines = longestOperationsSortedByModule.get(moduleName);
+        assertNotNull(logLines);
+        assertThat(logLines, not(empty()));
+        logLines.stream()
+                .findFirst()
+                .ifPresent(logLine -> {
+                    assertThat(logLine.getOperation(), is(equalTo(operation)));
+                    assertThat(logLine.getExecutionTimeMsec(), is(equalTo(executionTime)));
+                });
+    }
 }
