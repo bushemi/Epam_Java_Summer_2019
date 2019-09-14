e21ebc92d0ae2c496df053cfece2caa9587a331f
Igor_Boldysh <bushemi2001@gmail.com>
2019-09-14 18:19:05 +0300

    HT-10. Only formatting in the file.

--- homework_10_regexp/src/main/java/com/epam/serializer/LogsToFileSerializerService.java
+++ homework_10_regexp/src/main/java/com/epam/serializer/LogsToFileSerializerService.java
@@ -5,6 +5,7 @@ import com.epam.LogLine;
 import java.util.List;
 
 public interface LogsToFileSerializerService {
+
     void serialize(String fileName, List<LogLine> lines);
 
     List<LogLine> deserialize(String fileName);
