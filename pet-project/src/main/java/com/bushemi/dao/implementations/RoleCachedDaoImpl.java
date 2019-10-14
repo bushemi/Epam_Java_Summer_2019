7e084b9659bec3c9d6ecbb2dfd2a12186298ef6b
Igor_Boldysh <bushemi2001@gmail.com>
2019-10-14 20:30:36 +0300

    Was marked primary bean for role dao

--- pet-project/src/main/java/com/bushemi/dao/implementations/RoleCachedDaoImpl.java
+++ pet-project/src/main/java/com/bushemi/dao/implementations/RoleCachedDaoImpl.java
@@ -3,12 +3,14 @@ package com.bushemi.dao.implementations;
 import com.bushemi.dao.entity.dictionaries.Role;
 import com.bushemi.dao.interfaces.RoleDao;
 import com.bushemi.service.interfaces.DbConnectionService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 @Repository
+@Primary
 public class RoleCachedDaoImpl implements RoleDao {
     private Map<Long, Role> rolesById = new HashMap<>();
     private List<Role> allRoles;
