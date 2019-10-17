4203071ba9651c308f8d2b1f8f641a4a21b3974d
Igor_Boldysh <bushemi2001@gmail.com>
2019-10-18 00:25:36 +0300

    Was added test for config creating.

---
+++ pet-project/src/test/java/com/bushemi/ContextTest.java
@@ -0,0 +1,19 @@
+package com.bushemi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class})
+public class ContextTest {
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
