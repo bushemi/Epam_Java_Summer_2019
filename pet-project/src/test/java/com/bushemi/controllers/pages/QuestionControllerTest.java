b8de7e77277fd7c4b369da46e8fca8891e35aa97
Igor_Boldysh <bushemi2001@gmail.com>
2019-10-20 01:22:27 +0300

    Fixed tests for question controller.

--- pet-project/src/test/java/com/bushemi/controllers/pages/QuestionControllerTest.java
+++ pet-project/src/test/java/com/bushemi/controllers/pages/QuestionControllerTest.java
@@ -1,10 +1,11 @@
-package com.bushemi.controllers;
+package com.bushemi.controllers.pages;
 
 import com.bushemi.AppConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -30,8 +32,24 @@ public class QuestionControllerTest {
     }
 
     @Test
-    public void question() throws Exception {
-        this.mockMvc.perform(get("/question"))
+    public void questionAdmin() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("role", "admin");
+        this.mockMvc.perform(get("/question").session(session))
                 .andExpect(view().name("question"));
+    }
+
+    @Test
+    public void questionStudentShouldFail() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("role", "student");
+        this.mockMvc.perform(get("/question").session(session))
+                .andExpect(status().is(302));
+    }
+
+    @Test
+    public void questionShouldFail() throws Exception {
+        this.mockMvc.perform(get("/question"))
+                .andExpect(status().is(302));
     }
 }
