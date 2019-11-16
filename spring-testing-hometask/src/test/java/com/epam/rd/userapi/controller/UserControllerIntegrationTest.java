62ef3d293136064a312370af837ba5ac0d6dca3b
Igor_Boldysh <bushemi2001@gmail.com>
2019-11-16 22:02:40 +0200

    #4 Was added integration test for user controller

---
+++ spring-testing-hometask/src/test/java/com/epam/rd/userapi/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.epam.rd.userapi.controller;
+
+import com.epam.rd.userapi.BasicSpringTest;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class UserControllerIntegrationTest extends BasicSpringTest {
+
+    @Test
+    public void getUsers() throws Exception {
+        //GIVEN
+
+        //WHEN
+        mockMvc.perform(get("/users")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(10)))
+                .andExpect(jsonPath("$[0].email", is("Sincere@april.biz")))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].username", is("Bret")));
+
+        //THEN
+
+    }
+}
